36fc31337ada52b7b6ae402e5addd9b07c656962
dee0518 <dehya-518@naver.com>
2023-02-11 16:36:51 +0900

    수정2

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <div>
       <div>
-        <Link href={'/TodoList'}>TodoList 1</Link>
+        <Link href={'/TodoList'}>TodoList 123</Link>
       </div>
       <div>
         <Link href={'/IssueList'}>IssueList</Link>
